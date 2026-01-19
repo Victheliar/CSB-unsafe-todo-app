443131bb8d87b075c7194e908cc7d4beabe2aff1
Victoria Khoreva <victheliar@gmail.com>
2026-01-19 14:33:54 +0200

    start working on the homepage

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello world!")
-
 # Create your views here.
+def homePageView(request):
+    return render(request, "index.html")
+
+def registerPageView(request):
+    return render(request, "register.html")
