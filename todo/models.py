856b17032f083d6df43531aa39abce1f6036d0fe
Victoria Khoreva <victheliar@gmail.com>
2026-01-26 15:52:19 +0200

    Added Account to models.py

--- todo/models.py
+++ todo/models.py
@@ -2,5 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    pass
+class Account(models.Model):
+    username = models.TextField(User, on_delete=models.CASCADE)
+    password = models.TextField(User)
