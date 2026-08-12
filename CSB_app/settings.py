c5eb265740233cf0bcccab3db562a2fc86f5aa3d
Victheliar <victheliar@gmail.com>
2026-08-12 18:31:57 +0300

    change and hide secret key from public repo + add user instructions

--- CSB_app/settings.py
+++ CSB_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from .secrets import SECRET_KEY
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0wz9(bh+cv3q&801bn)p#zknptri8^um)*v_)gjj1-om4rgq87'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CSB-unsafe-todo-app
 A todo-app intentionally made with cyber security flaws for Mooc's Cyber Security Base course.
+
+# Instructions for running the project locally
+
+1. Clone the repository onto your machine with ```git clone git@github.com:Victheliar/CSB-unsafe-todo-app.git```
+
+2. Open the project and make a ```secrets.py``` file inside the CSB_app directory. Inside of this file, add a ```SECRET_KEY``` of your choosing.
+
+3. Now you can install and run the project locally using the following command:
+```python manage.py runserver```
