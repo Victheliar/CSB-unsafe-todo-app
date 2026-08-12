276ed9911c7af37bc4bab20f93a2d64d5e568200
Victheliar <victheliar@gmail.com>
2026-08-12 20:04:39 +0300

    added old code for comparison

--- CSB_app/settings.py
+++ CSB_app/settings.py
@@ -21,6 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-0wz9(bh+cv3q&801bn)p#zknptri8^um)*v_)gjj1-om4rgq87'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+
+# ALLOWED_HOSTS = []
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
