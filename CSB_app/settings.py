679aaa0edf13bd98b3738ef16ac88a00fa997131
Victheliar <victheliar@gmail.com>
2026-08-12 19:28:36 +0300

    set debug mode to false

--- CSB_app/settings.py
+++ CSB_app/settings.py
@@ -21,13 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
